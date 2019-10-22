e167d249c837c444821d08141bea6111202efa46
Jaskaran Singh <sjaskaran10@yandex.com>
2019-10-22 16:53:01 +0530

    basic layout ready

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,30 @@
 import React from "react";
 
 function Header() {
+  function navSlide() {
+    document.querySelector(".nav-links").classList.toggle("nav-active");
+  }
+
   return (
     <div id="header">
-      <h3>Home</h3>
-      <h3>Resume</h3>
-      <h3>...</h3>
-      <h3>Contact</h3>
+      <ul className="nav-links">
+        <li>
+          <i class="fas fa-home"></i>
+          <div>Home</div>
+        </li>
+        <li>
+          <i class="far fa-file"></i>
+          <div>Resume</div>
+        </li>
+        <li>
+          <i class="fas fa-mobile"></i>
+          <div>Contact</div>
+        </li>
+      </ul>
+      <div id="mobile-menu">
+        <i class="fas fa-bars" onClick={navSlide}></i>
+        <p>Portfolio</p>
+      </div>
     </div>
   );
 }
